058d41dff07a4ef2b096352e6c7da1f6d50a9ed4
Peet van de Sande <peet@peetvandesande.com>
2025-07-17 14:28:34 +0200

    Updated urls

--- install.ps1
+++ install.ps1
@@ -1,11 +1,11 @@
 # Download the handler
-$url = "https://gist.githubusercontent.com/thorstenkramm/b25a2c09ca7414595d48d1db581833fc/raw/1fecf170378390eebe778209a8b88972d6893657/ssh-protocol-handler.ps1"
+$url = "https://raw.githubusercontent.com/peetvandesande/windows-ssh-protocol-handler/refs/heads/master/ssh-protocol-handler.ps1"
 $file = "$env:LOCALAPPDATA\ssh-protocol-handler.ps1"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri $url -OutFile $file
 
 # Download the registry keys
-$url = "https://gist.githubusercontent.com/thorstenkramm/b25a2c09ca7414595d48d1db581833fc/raw/1fecf170378390eebe778209a8b88972d6893657/ssh-protocol-handler.reg"
+$url = "https://raw.githubusercontent.com/peetvandesande/windows-ssh-protocol-handler/refs/heads/master/add-ssh-handler.reg"
 $file = "$env:LOCALAPPDATA\ssh-protocol-handler.reg"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri $url -OutFile $file
